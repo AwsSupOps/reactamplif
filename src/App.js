686db4bc97cf86d23fb0d313a9261fc8859e51ad
Desco <gbalouama@gmail.com>
2022-12-15 18:41:30 -0600

    src com

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to master branch
+          Edit <code>src/App.js</code> and save to master branch pjjjjj
         </p>
         <a
           className="App-link"
