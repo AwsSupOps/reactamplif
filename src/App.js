41a00eeb7eaa5e09201bc296ef3129843c991700
Desco <gbalouama@gmail.com>
2022-12-15 18:46:45 -0600

    sr com

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to master branch pjjjjj
-        </p>
+          Edit <code>src/App.js</code> and save to masterLOL        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
